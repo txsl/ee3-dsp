7ca325d155190da58d76312fc18730dc5e4bdce5
Thomas Lim <txl11@ic.ac.uk>
2013-12-23 16:24:01 +0000

    ex 1.1 done

---
+++ 1/one.m
@@ -0,0 +1,18 @@
+n = -20:20;
+x = 0.5*dirac(n-5);
+stem(n, x);
+
+figure(2);
+n = 0:50;
+x = cos(n*pi/sqrt(23));
+stem(n, x);
+
+figure(3);
+n = 0:20;
+x = 0.8.^n;
+stem(n, x);
+
+figure(4);
+n = -10:10;
+x = exp(i*2*pi*n/10);
+stem(n, x);
